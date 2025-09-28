93f6f38630534fc64793f26d2e96f5c861dd514f
Yonatan Graber <yonatan.graber@gmail.com>
2025-09-27 22:13:47 -0700

    Add GitHub Actions workflow for deploying to GitHub Pages and update Vite config for base path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+    ],
+    base: '/civic-test/'
 })
